cb80b4d1464b246c4835bd8505a1ae93cbdb377a
Roman Alicando <romanaaav@gmail.com>
2024-10-28 16:26:48 -0300

    Java initial code.

---
+++ src/main/java/org/alicandor/springkerberospoc/SpringKerberosPocApplication.java
@@ -0,0 +1,13 @@
+package org.alicandor.springkerberospoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKerberosPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKerberosPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alicandor/springkerberospoc/SpringKerberosPocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alicandor.springkerberospoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKerberosPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
